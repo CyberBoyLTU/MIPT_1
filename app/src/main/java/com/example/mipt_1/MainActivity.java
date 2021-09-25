8654aafa0fc5fc30b2092f1970095f6e7b677654
CyberBoyLTU <91384210+CyberBoyLTU@users.noreply.github.com>
2021-09-25 20:04:52 +0300

    Button and TextView added.

--- app/src/main/java/com/example/mipt_1/MainActivity.java
+++ app/src/main/java/com/example/mipt_1/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.mipt_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView Message;
+    Button ShowMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting the variables to the Layout file
+        Message = findViewById(R.id.Message);
+        ShowMessage = findViewById(R.id.ShowMessage);
+
+        //Setting the OnClick Listener
+        ShowMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // What we want to display when button is clicked
+                Message.setText("Sveiki atvykę!");
+            }
+        });
     }
 }
