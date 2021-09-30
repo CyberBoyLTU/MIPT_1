e9b6e78eca94ee9e99e1b489c10eed0ac4764fe1
CyberBoyLTU <91384210+CyberBoyLTU@users.noreply.github.com>
2021-09-30 19:16:52 +0300

    2021-09-30

--- app/src/main/java/com/example/mipt_1/MainActivity.java
+++ app/src/main/java/com/example/mipt_1/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.mipt_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
 
     TextView Message;
     Button ShowMessage;
-    Button ChangeColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         //setting the variables to the Layout file
         Message = findViewById(R.id.Message);
         ShowMessage = findViewById(R.id.ShowMessage);
-        ChangeColor = findViewById(R.id.ChangeColor);
 
         //Setting the OnClick Listener
         ShowMessage.setOnClickListener(new View.OnClickListener() {
@@ -30,15 +26,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // What we want to display when button is clicked
                 Message.setText("Sveiki atvykę!");
-            }
-        });
-
-        ChangeColor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // Change color of the message
-                Message.setTextColor(Color.RED);
             }
         });
     }
